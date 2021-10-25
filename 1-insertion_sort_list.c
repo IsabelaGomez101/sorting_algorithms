52aa073e567edef9045b701929f309f43b404325
IsabelaGomez101 <3379@holbertonschool.com>
2021-10-25 01:02:52 -0500

    adding file insertion sort list

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,43 @@
+#include "sort.h"
+
+
+/**
+ * insertion_sort_list - function that sorts a doubly linked list of
+ * integers in ascending order using the Insertion sort algorithm
+ * @list: doubly linked list.
+ * Return: void.
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *temp = *list;
+	listint_t *current = *list;
+	listint_t *aux;
+
+	if (list != NULL)
+	{
+	current = current->next; /*starts with the second node in the list*/
+	while (current != NULL) /*loop doubly linked list*/
+	{
+		temp = current; /*copy current node*/
+		while (temp->prev != NULL && temp->n < temp->prev->n)
+		/*loop stops when it finds a greater n element*/
+		{
+			if (temp->next != NULL)
+			temp->next->prev = temp->prev;
+			if (temp->prev->prev != NULL)
+			temp->prev->prev->next = temp;
+			else
+			*list = temp;
+			/*swap nodes*/
+			aux = temp->prev->prev;
+			temp->prev->next = temp->next;
+			temp->next = temp->prev;
+			temp->prev->prev = temp;
+			temp->prev = aux;
+			print_list(*list);
+		}
+	    current = current->next;
+	}
+}
+}
